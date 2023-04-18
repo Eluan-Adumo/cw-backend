7c7d65092ab7a7248c438face109617f95ab5b1f
Eluan-Adumo <el1harolds@gmail.com>
2023-04-18 12:05:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -455,13 +455,13 @@ app.post("/api/submit-form", (req, res) => {
 
    const mailOptions = {
      from: process.env.CF_HOST,
-     to: receivedItems.data.email,
+     to: receivedItems.email,
      subject: "Quotation request from " + receivedItems.name,
      text: "Welcome to our Careful Watchers !,\n\nWe are committed to providing top-notch cybersecurity solutions to protect your business from potential cyber threats. Our team of experts is dedicated to keeping your sensitive data and assets safe from unauthorized access.\n\n If you are interested in learning more about our services, we would be happy to connect with you. Depending on your preference, we can either set up a call with one of our consultants or a Zoom meeting to discuss your specific needs and how we can help you achieve your cybersecurity goals. \n\n Please feel free to reach out to us at any time to schedule a call or Zoom meeting or request for our service quotation. \n\n We look forward to hearing from you and helping you secure your business against cyber threats. \n\nSincerely,\n\nTope Daisi\nCareful Watchers",
    };
 
    const mailOptionsSecond = {
-     from: receivedItems.data.email,
+     from: receivedItems.email,
      to: process.env.CF_HOST,
      subject: "Quotation request from " + receivedItems.name,
      text: req.body.message,
